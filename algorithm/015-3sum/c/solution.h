c7b310b4a80b86fcc00f366a5fe842b0451893af
Philon <id@philon.cn>
2018-12-21 20:54:26 +0800

    ✏️ update algo-015

--- algorithm/015-3sum/c/solution.h
+++ algorithm/015-3sum/c/solution.h
@@ -1,7 +1,50 @@
+#include <stdlib.h>
+#include <stdio.h>
 /**
  * Return an array of arrays of size *returnSize.
  * Note: The returned array must be malloced, assume caller calls free().
  */
 int** threeSum(int* nums, int numsSize, int* returnSize) {
-    
+  int** results = calloc(numsSize * 3, sizeof(int*));
+  int len = 0;
+
+  for (int i = 0; i < numsSize; i++) {
+    for (int j = i + 1; j < numsSize; j++) {
+      int a = nums[i] + nums[j];
+      for (int k = j + 1; k < numsSize; k++) {
+        if (a + nums[k]) {
+          continue;
+        }
+
+        int min = nums[i] < nums[j] ? nums[i] : nums[j];
+        min = min < nums[k] ? min : nums[k];
+        int max = nums[i] > nums[j] ? nums[i] : nums[j];
+        max = max > nums[k] ? max : nums[k];
+        int mid = nums[i];
+        if (min < nums[j] && nums[j] < max) mid = nums[j];
+        if (min < nums[k] && nums[k] < max) mid = nums[k];
+        if (nums[j] == nums[k]) mid = nums[j];
+
+        int equal = 0;
+        for (int l = 0; l < len; l++) {
+          if (min == results[l][0] && max == results[l][2]) {
+            equal = 1;
+          }
+        }
+
+        if (equal) {
+          continue;
+        }
+
+        results[len] = calloc(3, sizeof(int));
+        results[len][0] = min;
+        results[len][1] = mid;
+        results[len][2] = max;
+        len++;
+      }
+    }
+  }
+
+  *returnSize = len;
+  return results;
 }
--- algorithm/015-3sum/readme.md
+++ algorithm/015-3sum/readme.md
@@ -12,3 +12,58 @@ Example:
   [-1, -1, 2]
 ]
 ```
+
+## 我的思路一、穷举
+似乎所有算法题都可以用穷举这种笨办法，我刚开始却没什么好的思路，但由于题目本身的限制，即使用穷举也不简单，所以大概说一下思路，首先需要注意题意：
+1. 要找出全部相加为0的组合，所以需要注意二维数组的分配
+2. 全部组合中要“去重”，就是说`[0, -1, 1]`和`[-1, 0, 1]`这两个组合应该被看作一个
+
+留意这两个限制之后，其实解题思路就不难了：
+1. 三层for循环嵌套，遍历每种组合的和是否为0
+2. 发现相加为0的组合后，做一次排序，并存到结果数组中
+3. 遍历当前的结果数组，查看是否存在重复
+
+下面的代码不全，因为我觉得意义不大，因为做得不够严谨，其次O(n³)的时间复杂度根本无法通过leetcode的时间限制，所以就算逻辑对了，提交也不通过。
+```c
+int** threeSum(int* nums, int numsSize, int* returnSize) {
+  int** results = calloc(numsSize * 3, sizeof(int*));
+  int len = 0;
+
+  for (int i = 0; i < numsSize; i++) {
+    for (int j = i + 1; j < numsSize; j++) {
+      for (int k = j + 1; k < numsSize; k++) {
+        // 检查是否符合三值和为零
+        if (nums[i] + nums[j] + nums[k]) {
+          continue;
+        }
+
+        // 将三个值排序
+        int min = ...
+        int mid = ...
+        int max = ...
+        
+        // 去重
+        int equal = 0;
+        for (int l = 0; l < len; l++) {
+          if (min == results[l][0] && max == results[l][2]) {
+            equal = 1;
+          }
+        }
+        if (equal) {
+          continue;
+        }
+
+        // 赋值
+        results[len] = calloc(3, sizeof(int));
+        ...
+        len++;
+      }
+    }
+  }
+
+  *returnSize = len;
+  return results;
+}
+```
+- 时间复杂度： O(n³)，三层for的悲剧
+- 空间复杂度： O(n³)，最坏情况下
